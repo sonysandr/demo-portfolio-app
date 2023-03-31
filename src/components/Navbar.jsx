f59892b9fb033ab5c4d191d1e70478f28cef764a
anandu <sonysandr@gmail.com>
2023-03-31 23:40:25 +0530

    Some more additions to the Navbar component and started with  Home component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes ,FaGithub,FaLinkedin, FaFacebook } from "react-icons/fa";
+import  {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
@@ -41,7 +43,35 @@ const Navbar = () => {
         <li className=" py-6 text-4xl ">Contact</li>
       </ul>
       {/* Social icons */}
-        
+        <div className="hidden  lg:flex fixed flex-col top-[35%] left-0 ">
+          <ul>
+            <li className=" w-[160px] h-[60px] flex justify-between item-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-lg">
+              <a className="w-full flex justify-between items-center text-gray-300" 
+               href="/">
+                Linkedin <FaLinkedin size={30}/>
+              </a>
+            </li>
+            <li className=" w-[160px] h-[60px] flex justify-between item-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] rounded-lg">
+              <a className="w-full flex justify-between items-center text-gray-300" 
+               href="/">
+                GitHub <FaGithub size={30}/>
+              </a>
+            </li>
+            <li className=" w-[160px] h-[60px] flex justify-between item-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] rounded-lg">
+              <a className="w-full flex justify-between items-center text-gray-300" 
+               href="/">
+                
+                Resume <BsFillPersonLinesFill size={30}/>
+              </a>
+            </li>
+            <li className=" w-[160px] h-[60px] flex justify-between item-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] rounded-lg">
+              <a className="w-full flex justify-between items-center text-gray-300" 
+               href="/">
+                Email <HiOutlineMail size={30}/>
+              </a>
+            </li>
+          </ul>
+        </div>
     </div>
   );
 };
